45c805732c790497dc7cddbbe1c637d07c6b56b1
TevesPAN99 <69859681+TevesPAN99@users.noreply.github.com>
2026-04-25 15:54:33 +0900

    Time: 3 ms (52.44%), Space: 20.6 MB (9.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}  # 键是值，值是下标
-        
+        seen = {}
+
         for i, num in enumerate(nums):
-            complement = target - num   # 我要找的"另一半"
-            
+            complement = target-num
             if complement in seen:
-                # 找到了！返回两个下标
-                return [seen[complement], i]
-            
-            # 没找到，把当前数和它的下标存进字典
-            seen[num] = i
+                return [seen[complement],i]
+            seen[num]=i
