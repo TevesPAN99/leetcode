6eda864d3ff2953b5105601cd791c1979215559c
TevesPAN99 <69859681+TevesPAN99@users.noreply.github.com>
2026-04-26 20:19:13 +0900

    Time: 12 ms (58.29%), Space: 22.1 MB (57.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,6 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
-        
         for s in strs:
-            # 创建长度26的计数数组
-            count = [0] * 26
-            for ch in s:
-                count[ord(ch) - ord('a')] += 1
-            # list 不能做字典的 key，转成 tuple
-            groups[tuple(count)].append(s)
-        
+            groups[''.join(sorted(s))].append(s)
         return list(groups.values())
